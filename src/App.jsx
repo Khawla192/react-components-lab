52918308e82ef73f00e0de0a7ceb622d1a9dc310
Khawla192 <202005171@stu.uob.edu.bh>
2025-01-27 18:05:48 +0300

    build a weather forecast page that re-uses a single component to display multiple days’ worth of weather forecasts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
+import './App.css';
+import WeatherForecast from './components/WeatherForecast/WeatherData/WeatherForecast.jsx';
+import WeatherIcon from './components/WeatherForecast/WeatherIcon/WeatherIcon.jsx';
+
 const App = () => {
+  const weatherForecasts = [
+    {
+      day: 'Mon',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg',
+      imgAlt: 'sun icon',
+      conditions: 'sunny',
+      time: 'Morning',
+    },
+    {
+      day: 'Tue',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg',
+      imgAlt: 'moon icon',
+      conditions: 'clear',
+      time: 'Night',
+    },
+    {
+      day: 'Wed',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg',
+      imgAlt: 'clouds with lightning icon',
+      conditions: 'stormy',
+      time: 'All Day',
+    },
+    {
+      day: 'Thu',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg',
+      imgAlt: 'sun overcast by clouds icon',
+      conditions: 'overcast',
+      time: 'Evening',
+    },
+    {
+      day: 'Fri',
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg',
+      imgAlt: 'moon overcast by clouds icon',
+      conditions: 'cloudy',
+      time: 'Night',
+    },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Local Weather</h1>
+      <section>
+      {weatherForecasts.map((weather, index) => {
+        return (
+          <WeatherForecast 
+          key={index} 
+          day={weather.day} 
+          img={weather.img} 
+          imgAlt={weather.imgAlt} 
+          conditions={weather.conditions} 
+          time={weather.time}/>
+          // <WeatherForecast key={index} weahter={...weather}/>
+        )
+      })}
+      </section>
+    </>
   );
 }
 
---
+++ src/components/WeatherForecast/WeatherIcon/WeatherIcon.jsx
@@ -0,0 +1,14 @@
+import './WeatherIcon.css';
+
+const WeatherIcon = (props) => {
+
+    const { img, imgAlt } = props
+    
+    return (
+        <div className="weather">
+            <img src={img} alt={imgAlt} />
+        </div>
+    );
+};
+
+export default WeatherIcon;
